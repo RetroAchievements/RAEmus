970ffbe47c1999cf86b4765157c79217e53212ce
Scott Davies <SDavies@mazooma.co.uk>
2014-10-09 13:35:08 +0100

    Debug test code for HTTP transactions...

--- bizhawk/BizHawk.Client.DiscoHawk/Properties/svnrev.cs
+++ bizhawk/BizHawk.Client.DiscoHawk/Properties/svnrev.cs
@@ -1,4 +1,4 @@
 static class SubWCRev
 {
-	public const string SVN_REV = "7542";
+	public const string SVN_REV = "0";
 }
--- bizhawk/BizHawk.Client.EmuHawk/Program.cs
+++ bizhawk/BizHawk.Client.EmuHawk/Program.cs
@@ -9,6 +9,9 @@ using SlimDX.DirectSound;
 using Microsoft.VisualBasic.ApplicationServices;
 #endif
 
+//  ##RA
+using System.Net.Http;
+
 #pragma warning disable 618
 
 using BizHawk.Common;
@@ -61,6 +64,47 @@ namespace BizHawk.Client.EmuHawk
 					return;
 				}
 			}
+
+
+            //  ##RA test code:
+
+            using (var client = new HttpClient())
+            {
+                const string BaseURL = "http://retroachievements.org/";
+                const string LoginPage = "requestlogin.php";
+                const string ActivityPage = "requestpostactivity.php";
+
+                string userName = "qwe";
+                string pass = "qwe";    //  Yuck. Plaintext?!?
+
+                var values = new List<KeyValuePair<string, string>>();
+
+                values.Clear();
+                values.Add(new KeyValuePair<string, string>("u", userName));
+                values.Add(new KeyValuePair<string, string>("p", pass));
+                
+                System.Threading.Tasks.Task<HttpResponseMessage> response = client.PostAsync(BaseURL + LoginPage, new FormUrlEncodedContent(values));
+                response.Wait();
+                var responseString = response.Result.Content.ReadAsStringAsync();
+                MessageBox.Show("Login result: " + responseString.Result);
+                if( responseString.Result.Substring( 0, 3 ) == "OK:" )
+                {
+                    string userToken = responseString.Result.Substring(3, 16);
+
+                    values.Clear();
+                    values.Add(new KeyValuePair<string, string>("u", userName));
+                    values.Add(new KeyValuePair<string, string>("t", userToken));
+                    values.Add(new KeyValuePair<string, string>("a", "3"));         //  activity type (3=playing)
+                    values.Add(new KeyValuePair<string, string>("m", "30"));        //  game ID ( 1 = Sonic (Mega Drive), 30 = Jet Force Gemini (N64) )
+
+                    response = client.PostAsync(BaseURL + ActivityPage, new FormUrlEncodedContent(values));
+                    response.Wait();
+                    responseString = response.Result.Content.ReadAsStringAsync();
+                    MessageBox.Show("Post Activity result: " + responseString.Result);
+                }
+            }
+
+
 
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
