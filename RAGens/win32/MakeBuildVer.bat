18d7e40faa4bbd32aa95b968717fc2a0936f424f
Scott Davies <SDavies@mazooma.co.uk>
2015-04-07 13:23:28 +0100

    updated versioning

--- RAGens/win32/MakeBuildVer.bat
+++ RAGens/win32/MakeBuildVer.bat
@@ -1,7 +1,9 @@
 @echo off
 
-git describe --tags --long > LiveTag.txt
+REM git describe --tags --long > LiveTag.txt
+git describe --tags --match "RAGens.*" > LiveTag.txt
 @set /p ACTIVE_TAG=<LiveTag.txt
+@set VERSION_NUM=%ACTIVE_TAG:~7,3%
 
 git diff HEAD > Diffs.txt
 @set /p RAW_DIFFS_FOUND=<Diffs.txt
@@ -11,5 +13,5 @@ REM set file=
 @for /F "usebackq" %%A in ('"Diffs.txt"') do set DIFF_FILE_SIZE=%%~zA
 @if %DIFF_FILE_SIZE% GTR 0 set ACTIVE_TAG=Unstaged Changes
 
-@echo Tag: %ACTIVE_TAG%
-@echo #define RAGENS_VERSION "%ACTIVE_TAG%" > ../../BuildVer.h
+@echo Tag: %ACTIVE_TAG% (%VERSION_NUM%)
+@echo #define RAGENS_VERSION "0.%VERSION_NUM%" > ../common/src/BuildVer.h
