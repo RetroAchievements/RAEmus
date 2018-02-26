589f7a4489e54e53b8e54531915ca56e3d2c9e95
Scott <Scott@RetroAchievements.org>
2018-02-26 13:38:39 +0000

    Deploy Script updates

--- DeployRANes.ps1
+++ DeployRANes.ps1
@@ -37,8 +37,8 @@
         $Credential = New-Object System.Management.Automation.PSCredential ('ec2-user', $Password)
         $KeyPath = ".\RetroAchievementsKey"
         $TargetURL = "www.RetroAchievements.org"
-        $WebRoot = "/var/www/html"
-        $WebRootBin = "/var/www/html/bin"
+        $WebRoot = "/var/www/html/public"
+        $WebRootBin = "/var/www/html/public/bin"
 
         if (-not (Test-Path "$env:ProgramFiles\7-Zip\7z.exe")) 
         {
